b2553fd3d889192f63c18656ea109e4c97499274
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-12-05 15:31:24 +0530

    add firebase

---
+++ my_project/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CrudComponent } from './crud/crud.component';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrudComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my_project/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+   firebaseConfig : {
+    apiKey: "AIzaSyCV3psf2yF8Cqjd-dIEeJv5aigYPO9-7XU",
+    authDomain: "angular-crud-ed5e2.firebaseapp.com",
+    databaseURL: "https://angular-crud-ed5e2-default-rtdb.firebaseio.com",
+    projectId: "angular-crud-ed5e2",
+    storageBucket: "angular-crud-ed5e2.appspot.com",
+    messagingSenderId: "860744461492",
+    appId: "1:860744461492:web:94e78c512c84fb266b7be1"
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
